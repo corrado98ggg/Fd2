ec52d6eb56a3ec1faf4e961888b43e8c61819d96
corrado98ggg <64374442+corrado98ggg@users.noreply.github.com>
2021-04-18 16:43:56 +0200

    Add files via upload

--- backtracking/babbonatale.c
+++ backtracking/babbonatale.c
@@ -1,52 +1,57 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
 
-void BabboNataleREC(const int* pacchi, size_t pacchi_size, int p, bool* vcurr, int sum, int i) {
+/*
+p:           portata massima della slitta;
+pacchi :     array dei pesi dei regali disponibili;
+n :          dimensione dell'array pacchi;
+i :          la posizione attuale;
+vcurr :      un array che indica i regali attualmente caricati nella soluzione corrente(ad esempio 1 = caricato, 0 = NON caricato);
+vbest:       un array che indica i regali caricati nella miglior soluzione fino ad ora trovata (ad esempio 1 = caricato, 0 = NON caricato);
+max:         numero di regali caricati nella soluzione best;
+cnt:         numero di regali caricati nella soluzione vcurr;
+sum:         somma dei pesi dei regali caricati nella soluzione vcurr;
+*/
 
-	//caso base:
-	if (i == pacchi_size) {
 
-		for (int j = 0; j < pacchi_size; j++) {
-			printf("%d ", vcurr[j]);
+void BabboNatale(int p, int const* pacchi, int n, unsigned i, bool* vcurr, bool* vbest, unsigned* max, unsigned int cnt, int sum) {
+
+	//caso base
+	if (i == n) {
+		if (cnt > (*max)) {
+			(*max) = cnt;
+			memcpy(vbest, vcurr, n * sizeof(bool));
 		}
-
-		return;
-	}
-
-	//caso carico il regalo
-	if (pacchi[i] < p && pacchi[i+1] + pacchi[i] < p && sum < p && sum + pacchi[i] < p) {
-		vcurr[i] = 1;
-		sum = sum + pacchi[i];
-		BabboNataleREC(pacchi, pacchi_size, p, vcurr, sum, i + 1);
 		return;
 	}
 
 	vcurr[i] = 0;
-	BabboNataleREC(pacchi, pacchi_size, p, vcurr, sum, i + 1);
-	return;
+	BabboNatale(p, pacchi, n, i + 1, vcurr, vbest, max, cnt, sum);
+
+
+	if (sum + pacchi[i] <= p) {
+		vcurr[i] = 1;
+		BabboNatale(p, pacchi, n, i + 1, vcurr, vbest, max, cnt + 1, sum + pacchi[i]);
+	}
 
 }
 
-void BabboNatale(const int* pacchi, size_t pacchi_size, int p) {
-	bool* vcurr = malloc(sizeof(bool) * pacchi_size);
-	int sum = 0;
-
-	BabboNataleREC(pacchi, pacchi_size, p, vcurr, sum, 0);
-	return;
-}
-
-
-/*
 int main() {
 
+	int p = 20;
+	int pacchi[5] = { 10,11,1,3,3 };
+	int n = 5;
+	bool* vcurr = malloc(sizeof(bool) * 5);
+	bool vbest[] = { 0,0,0,0,0 };
+	int max[] = { 0,0,0,0,0 };
+	unsigned cnt = 0;
+	int sum = 0;
+	int i = 0;
 
-	int pacchi[] = {  }; //kg dell'i-esimo pacco
-	size_t pacchi_size = 5;
-	BabboNatale(pacchi, pacchi_size, 20);
+	BabboNatale(p, pacchi, n, i, vcurr, vbest, max, cnt, sum);
+
 	return 0;
-
-
 }
-*/
